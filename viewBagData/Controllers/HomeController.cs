4d333230a4b9e9754cb7cdc522c84bf72aef5073
saif-mhr <saifullah786@hotmail.com>
2020-12-25 00:54:31 +0500

    Add project files.

---
+++ viewBagData/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using viewBagData.Models;
+
+namespace viewBagData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.var1 = "im from Controller with using viewBag";
+            ViewBag.var2 = DateTime.Now.ToLongDateString();
+            string[] str = { "saif", "ali", "mahar" };
+            ViewBag.var3 = str;
+
+            ViewBag.var4 = new List<string>()
+            {
+                "cricket",
+                "Football",
+                "hockey",
+            };
+
+            Employee emp = new Employee();
+            emp.name = "saifullah";
+            emp.designation = "lectural";
+            emp.city = "hyderabad";
+
+            ViewBag.var5 = emp;
+            return View();
+        }
+    }
+}
---
+++ viewBagData/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace viewBagData.Models
+{
+    public class Employee
+    {
+        public string name { get; set; }
+        public string designation { get; set; }
+
+        public string city { get; set; }
+    }
+}
